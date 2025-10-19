09e2da796c6d678911964f49c034a017b196c686
SamiReg <samireg2002@gmail.com>
2025-10-19 01:44:41 +0100

    Contact added, added Js for highlighting the selected menu element, added hover effect on links and cards, in headerand footer too, corrected some copying mistakes between pages, this message might be reworded like the previous one if needed later

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,32 @@ function copyRightYear() {
 }
 
 copyRightYear();
+
+function updateSelectedMenuItem() {
+    const menuItemsFullPaths = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
+    // const menuItems = Array.from(menuItemsFullPaths).map(item => item.getAttribute('href'));
+    const selected = window.location.pathname.split('/').pop();
+
+    // console.log('Menu items:', menuItems);
+    // console.log('Selected path:', selected);
+
+    if (selected === '' || selected === '/') {
+        // console.log('[0]' + menuItems[0]);
+        menuItemsFullPaths[0].classList.add('selected');
+        return;
+    }
+
+    for (let item of menuItemsFullPaths) {
+        if (item.getAttribute('href') === selected) {
+            console.log('Selected menu item:', item.getAttribute('href'));
+            item.classList.add('selected');
+        }
+        else {
+            item.classList.remove('selected');
+        }
+    }
+
+    // console.log('END');
+}
+
+updateSelectedMenuItem();
